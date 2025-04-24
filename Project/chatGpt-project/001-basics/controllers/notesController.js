228da603160afdb04a66a75471e0914cf2b40700
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-04-24 12:56:35 +0100

    setting up environment for JWT security and user auth

--- Project/chatGpt-project/001-basics/controllers/notesController.js
+++ Project/chatGpt-project/001-basics/controllers/notesController.js
@@ -1,43 +1,43 @@
 const Note = require("../models/Note");
-
+const asyncHandler = require("../middleware/asyncHandler");
 // let notes = [
 //   { id: 1, content: "Learn Express.js" },
 //   { id: 2, content: "Understand APIs" },
 // ];
 
 // Get all notes
-exports.getAllNotes = async (req, res) => {
+exports.getAllNotes = asyncHandler(async (req, res) => {
   const notes = await Note.find();
   res.json(notes);
-};
+});
 
 // Get a note by ID
-exports.getNoteById = async (req, res) => {
+exports.getNoteById = asyncHandler(async (req, res) => {
   const note = await Note.findById(req.params.id);
   if (!note) {
     return res.status(404).send({ message: "Note not found" });
   }
   res.json(note);
-};
+});
 
 // Post a new note
-exports.createNote = async (req, res) => {
+exports.createNote = asyncHandler(async (req, res) => {
   const note = new Note({ content: req.body.content });
   await note.save();
   res.status(201).json(note);
-};
+});
 
 // Delete note
-exports.deleteNote = async (req, res) => {
+exports.deleteNote = asyncHandler(async (req, res) => {
   const note = await Note.findByIdAndDelete(req.params.id);
   if (!note) {
     return res.status(404).json({ message: "Note not found" });
   }
   res.json({ messgae: "Noted deleted" });
-};
+});
 
 // Put note
-exports.updateNote = async (req, res) => {
+exports.updateNote = asyncHandler(async (req, res) => {
   const note = await Note.findByIdAndUpdate(
     req.params.id,
     { content: req.body.content },
@@ -47,4 +47,4 @@ exports.updateNote = async (req, res) => {
     return res.status(404).json({ messgae: "Note not found" });
   }
   res.json(note);
-};
+});
