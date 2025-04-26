66febbb1314e7fe037067b2da51969e80317e7b3
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-04-26 18:54:32 +0100

    add function of fetching add and delete on frontend

--- Project/chatGpt-project/frontends/src/App.js
+++ Project/chatGpt-project/frontends/src/App.js
@@ -4,59 +4,54 @@ import axios from "axios";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const url = "http://localhost:5000/tasks";
 
   // Fetch tasks from the backend
   useEffect(() => {
     axios
-      .get("http://localhost:5000/tasks")
-      .then((response) => setTasks(response.data))
+      .get(url)
+      .then((res) => setTasks(res.data))
       .catch((error) => console.log("Error fetching tasks", error));
   }, []);
 
   //Add a new Tsk
+  const addTask = () => {
+    axios
+      .post(url, { name, newTask })
+      .then((res) => setTasks([...tasks, res.data]))
+      .catch((err) => console.log("Error adding task", err));
+    setNewTask("");
+  };
+
+  // Delete a task
+  const deleteTask = (id) => {
+    axios
+      .delete(`${url}/${id}}`)
+      .then(() => setTasks(tasks.filter((task) => task._id !== id)))
+      .catch((err) => console.log("Error deleting task", err));
+  };
 
   return (
     <div>
       <h1> Todo-List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="New task"
+      />
+
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>
+            {task.name}
+            <button onClick={() => deleteTask(task._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-//   // Add a new task
-//   const addTask = () => {
-//     axios.post('http://localhost:5000/tasks', { name: newTask })
-//       .then(response => setTasks([...tasks, response.data]))
-//       .catch(error => console.log('Error adding task', error));
-//     setNewTask('');
-//   };
-
-//   // Delete a task
-//   const deleteTask = (id) => {
-//     axios.delete(`http://localhost:5000/tasks/${id}`)
-//       .then(() => setTasks(tasks.filter(task => task._id !== id)))
-//       .catch(error => console.log('Error deleting task', error));
-//   };
-
-//   return (
-//     <div>
-//       <h1>To-Do List</h1>
-//       <input
-//         type="text"
-//         value={newTask}
-//         onChange={e => setNewTask(e.target.value)}
-//         placeholder="New task"
-//       />
-//       <button onClick={addTask}>Add Task</button>
-//       <ul>
-//         {tasks.map(task => (
-//           <li key={task._id}>
-//             {task.name}
-//             <button onClick={() => deleteTask(task._id)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
 
 export default App;
