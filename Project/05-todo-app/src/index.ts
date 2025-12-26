52d2f63d00d98befb1082c5f3bc2bfc884799449
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-12-26 15:49:12 +0100

    married implemetations with zod

--- Project/05-todo-app/src/index.ts
+++ Project/05-todo-app/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { z } from "zod";
 
 const app = express();
 
@@ -8,8 +9,20 @@ const PORT = 3000;
 type Todo = {
   id: string;
   title: string;
-  completed: boolean;
+  priority?: string;
+  completed?: boolean;
 };
+
+const CreateTodoSchema = z.object({
+  id: z.string().optional(),
+  title: z
+    .string("Title is required")
+    .min(2, "Title should be at least 2 characters"),
+  priority: z.enum(["low", "urgent", "emergency"]).default("low"),
+  completed: z.boolean().default(false),
+});
+// type Todo = z.infer<typeof TodoSchema>;
+
 const todos: Todo[] = [];
 
 app.get("/", (req, res) => {
@@ -17,11 +30,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/todos", (req, res) => {
-  const { title } = req.body;
+  const parseResult = CreateTodoSchema.safeParse(req.body);
+
+  if (!parseResult.success) {
+    return res.status(400).json({
+      message: "Invalid request data",
+      errors: parseResult.error.format(),
+    });
+  }
+  const { title, priority } = parseResult.data;
 
   const newTodo: Todo = {
     id: Date.now().toString(),
     title,
+    priority,
     completed: false,
   };
   todos.push(newTodo);
@@ -31,7 +53,7 @@ app.post("/todos", (req, res) => {
 });
 
 app.patch("/todos/:id", (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
 
   const todo = todos.find((t) => t.id === id);
   if (!todo) {
@@ -41,6 +63,18 @@ app.patch("/todos/:id", (req, res) => {
   console.log("Todos after toggle:", todos);
 });
 
+app.delete("/todos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = todos.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  const deletedTodo = todos.splice(index, 1);
+
+  console.log("Todos after delete:", todos);
+  res.json(deletedTodo[0]);
+});
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
